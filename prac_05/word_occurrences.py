ab7264eec5b71ac8abe221ddc4794cc8906d16cd
Admin <trandoan.bui@my.jcu.edu.au>
2025-03-02 15:47:23 +0800

    Word occurrences exercise with estimated and actual time

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,15 @@
+"""
+Word Occurrences
+Estimate: 15 minutes
+Actual:   17 minutes
+"""
+string = input("Text: ")
+words = string.split()
+word_counts = {}
+
+for word in words:
+    word_counts[word] = word_counts.get(word, 0) + 1
+sorted_words = sorted(word_counts.keys())
+max_length = max(len(word) for word in sorted_words)
+for word in sorted_words:
+    print(f"{word:{max_length}} : {word_counts[word]}")
