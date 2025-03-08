7c3ba35e6d9cd334db3245732dc7b554b6283ffd
Admin <trandoan.bui@my.jcu.edu.au>
2025-03-09 06:33:17 +0800

    Add limo car object, modify fuel, print details, and attempt drive

--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -13,5 +13,13 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+    # Create a new Car object named "limo" with 100 fuel
+    limo = Car(100)
+    # Add 20 more fuel
+    limo.add_fuel(20)
+    # Print fuel amount
+    print(f"Limo has fuel: {limo.fuel}")
 
+    # Attempt to drive 115 km
+    limo.drive(115)
 main()
