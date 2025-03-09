54eb284ab55a6dd348baf00f27e0f66895d4f2fd
Admin <trandoan.bui@my.jcu.edu.au>
2025-03-09 18:55:07 +0800

    Added predefined test data for quicker testing in guitars.py.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,22 +8,13 @@ from guitar import Guitar
 
 
 def main():
-    """Collect and display a list of guitars from user input."""
-
+    """Manage and display a collection of guitars."""
     print("My guitars!")
-    guitars = []
-
-    # Get user input
-    while True:
-        name = input("Name: ")
-        if name == "":
-            break
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-        print(f"{guitar} added.\n")
-
+    guitars = [
+        Guitar("Gibson L-5 CES", 1922, 16035.40),
+        Guitar("Line 6 JTV-59", 2010, 1512.90),
+        Guitar("Fender Stratocaster", 2014, 765.40)
+    ]
     # Display all guitars
     print("\nThese are my guitars:")
     for i, guitar in enumerate(guitars, 1):
