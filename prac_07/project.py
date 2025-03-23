2e71be8a44e70ac839afaf477c13c817588bb26d
Admin <trandoan.bui@my.jcu.edu.au>
2025-03-23 19:47:23 +0800

    Added Project class with attributes

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,3 +3,12 @@ Prac 07 Project Management Program
 Estimate time: 2 Hours
 Actual time:
 """
+import datetime
+
+class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
