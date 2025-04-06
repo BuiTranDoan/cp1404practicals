be8958f22466dbfeb70ea9b188d8bc150dc4ba4d
Admin <trandoan.bui@my.jcu.edu.au>
2025-04-06 20:06:06 +0800

    Add SilverServiceTaxi class inheriting from Taxi with fanciness and flagfall

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,20 @@
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """A special taxi with fanciness and a flagfall charge."""
+
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi instance."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness  # Customize fare per km
+
+    def get_fare(self):
+        """Return the fare including flagfall, rounded to nearest 10c."""
+        return round(super().get_fare() + self.flagfall, 1)
+
+    def __str__(self):
+        """Return string representation of the taxi with flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
